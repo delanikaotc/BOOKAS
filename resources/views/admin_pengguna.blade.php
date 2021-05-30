e25ef77a0670cdfd5d834413b57363cebe058a38
alvaniakrn <alvinavaniakir@yahoo.co.id>
2021-05-30 23:36:49 +0700

    database

--- resources/views/admin_pengguna.blade.php
+++ resources/views/admin_pengguna.blade.php
@@ -50,7 +50,6 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                    @php dd($users) @endphp
                                         @forelse($users as $user)
                                         <tr>
                                             <td>{{ $user->id }}.</td>
