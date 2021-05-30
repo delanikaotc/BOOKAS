0cdb463bd2104ac0101792e5df0022b0d72455dc
alvaniakrn <alvinavaniakir@yahoo.co.id>
2021-05-30 21:02:07 +0700

    database

--- database/migrations/2014_10_12_000000_create_user_table.php
+++ database/migrations/2014_10_12_000000_create_user_table.php
@@ -14,7 +14,7 @@ class CreateUserTable extends Migration
     public function up()
     {
         Schema::create('user', function (Blueprint $table) {
-            $table->id()->nullable;
+            $table->id();
             $table->string('name');
             $table->string('email');
             $table->string('password');
