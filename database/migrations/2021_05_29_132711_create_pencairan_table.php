6d7731e134ada90ba4fa7c0fe3c4577548d6b79d
alvaniakrn <alvinavaniakir@yahoo.co.id>
2021-05-30 20:01:07 +0700

    database

--- database/migrations/2021_05_29_132711_create_pencairan_table.php
+++ database/migrations/2021_05_29_132711_create_pencairan_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePencarianTable extends Migration
+class CreatePencairanTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePencarianTable extends Migration
      */
     public function up()
     {
-        Schema::create('pencarian', function (Blueprint $table) {
+        Schema::create('pencairan', function (Blueprint $table) {
             $table->id();
             $table->integer('id_user')->nullable();
             $table->string('no_transaksi')->nullable();
@@ -32,6 +32,6 @@ class CreatePencarianTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pencarian');
+        Schema::dropIfExists('pencairan');
     }
 }
